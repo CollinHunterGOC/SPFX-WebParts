c3ca59e426988d04e3fce1bc510aecf28fed68c0
CollinHunterGOC <143524517+CollinHunterGOC@users.noreply.github.com>
2025-10-31 15:23:05 -0300

    Add gulpfile.js for SPFx project

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,25 @@
 const gulp = require('gulp');
-const build = require('@microsoft/sp-build-web');
 
-build.initialize(gulp);
+try {
+  const build = require('@microsoft/sp-build-web');
+  try {
+    const ver = require('@microsoft/sp-build-web/package.json').version;
+    console.log(`Loaded @microsoft/sp-build-web v${ver}`);
+  } catch (e) {
+    console.log('Loaded @microsoft/sp-build-web (version unknown)');
+  }
+
+  build.initialize(gulp);
+
+  // Provide a convenience alias: `gulp serve` -> `gulp serve-deprecated`
+  try {
+    gulp.task('serve', gulp.series('serve-deprecated'));
+  } catch (err) {
+    // ignore if registration fails for some reason
+  }
+} catch (err) {
+  console.error('ERROR: Failed to load @microsoft/sp-build-web. Details below:\n');
+  console.error(err && err.stack ? err.stack : err);
+  console.error('\nMake sure dependencies are installed (npm install) and you are running gulp from the project root.');
+  process.exitCode = 1;
+}
